8d8ca7fa56a24ebf539534e7d65ce53216cf0791
Ben <bschreiber91@cs.huji.ac.il>
2021-05-24 12:16:12 +0300

    Add DB model

---
+++ Backend/models.py
@@ -0,0 +1,40 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class Sefer(db.Model):  # e.g Tanach
+
+    name = db.Column(db.String(), primary_key=True)
+
+    def __repr__(self) -> str:
+        pass
+
+
+class SeferSub1(db.Model):  # e.g Torah
+
+    name = db.Column(db.String(), primary_key=True)
+    parent_name = db.Column(db.String(), db.ForeignKey('Sefer.name'))
+
+    def __repr__(self) -> str:
+        pass
+
+
+class SeferSub2(db.Model):  # e.g Sh'mot
+
+    name = db.Column(db.String(), primary_key=True)
+    parent_name = db.Column(db.String(), db.ForeignKey('SeferSub1.name'))
+
+    def __repr__(self) -> str:
+        pass 
+
+class ChapterPage(db.Model):  # e.g Perek
+
+    number = db.Column(db.Integer, primary_key=True)
+    parent_name = db.Column(db.String(), db.ForeignKey('SeferSub2.name'))
+    chapter_page_type = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean(), default=False)
+
+    def __repr__(self) -> str:
+        pass
