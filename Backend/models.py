19265cd7112a22b1409c2c89b614b4cfc9aa37e6
Ben <bschreiber91@cs.huji.ac.il>
2021-05-24 17:00:06 +0300

    Refactor DB models to be recursive

--- Backend/models.py
+++ Backend/models.py
@@ -1,46 +1,53 @@
 from flask_sqlalchemy import SQLAlchemy
+from typing import Dict
 
 db = SQLAlchemy()
 
 
-class Sefer(db.Model):  # e.g Tanach
+class Section(db.Model):
+    __tablename__ = 'section'
 
-    name = db.Column(db.String(), primary_key=True)
-    total_items = db.Column(db.Integer(), nullable=False)
-    total_completed = db.Column(db.Integer(), default=0)
-
-    def __repr__(self) -> str:
-        pass
-
-
-class SeferSub1(db.Model):  # e.g Torah
-
-    name = db.Column(db.String(), primary_key=True)
-    parent_name = db.Column(db.String(), db.ForeignKey('Sefer.name'))
-    total_items = db.Column(db.Integer(), nullable=False)
-    total_completed = db.Column(db.Integer(), default=0)
+    name = db.Column(db.String(50), primary_key=True)
+    parent_name = db.Column(db.String(50), db.ForeignKey('section.name'))
+    children = db.relationship("Section")
+    total_completed = db.Column(db.Integer, default=0)
+    num_items = db.Column(db.Integer, nullable=False)
+    section_type = db.Column(db.String(50), nullable=False)
+    is_lowest = db.Column(db.Boolean, default=False, nullable=False)  # Is the lowest Sefer section before reaching ChapterPage?
 
     def __repr__(self) -> str:
-        pass
+        return f'Section(name={self.name}, parent_name={self.parent_name})'
+
+    def serialize(self) -> Dict[str, db.Column]:
+        return {
+            "name" : self.name,
+            "parent_name" : self.parent_name,
+            "children" : self.children,
+            "total_completed" : self.total_completed,
+            "num_items" : self.num_items,
+            "section_type" : self.section_type,
+            "is_lowest" : self.is_lowest,
+        }
 
 
-class SeferSub2(db.Model):  # e.g Sh'mot
-
-    name = db.Column(db.String(), primary_key=True)
-    parent_name = db.Column(db.String(), db.ForeignKey('SeferSub1.name'))
-    total_items = db.Column(db.Integer(), nullable=False)
-    total_completed = db.Column(db.Integer(), default=0)
-
-    def __repr__(self) -> str:
-        pass
-
-
-class ChapterPage(db.Model):  # e.g Perek
+class ChapterPage(db.Model):  # e.g Perek/Daf
+    __tablename__ = 'chapter_page'
 
     number = db.Column(db.Integer, primary_key=True)
-    parent_name = db.Column(db.String(), db.ForeignKey('SeferSub2.name'))
-    chapter_page_type = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean(), default=False)
+    parent_name = db.Column(db.String(50), db.ForeignKey('sefer.name'))
+    page_type = db.Column(db.String(), nullable=False)
+    is_completed = db.Column(db.Boolean(), default=False)
 
     def __repr__(self) -> str:
-        pass
+        return f'ChapterPage({self.page_type} {self.number}, completed={self.is_completed}'
+
+    def __str__(self) -> str:
+        return self.page_type + ' ' + str(self.number)
+
+    def serialize(self) -> Dict[str, db.Column]:
+        return {
+            "number" : self.number,
+            "parent_name" : self.parent_name,
+            "page_type" : self.page_type,
+            "is_completed" : self.is_completed,
+        }
